098a027de65cd7b98fcd0441bcdca98ac6cef62f
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-01-03 13:03:47 +0000

    feat: add dining philosophers solution

---
+++ dining_philosophers/src/main.rs
@@ -0,0 +1,70 @@
+use std::thread;
+use std::sync::{Mutex, Arc};
+
+// A struct is like a Class in the ES6 or Python world, this is how we
+// initialise it
+struct Philosopher {
+    name: String,
+    left: usize,
+    right: usize,
+}
+
+// We then implement the methods and such we would like on the Struct
+// 'new' is an associated function
+impl Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+            left: left,
+            right: right,
+        }
+    }
+
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep_ms(150);
+        let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} is eating.", self.name);
+
+        thread::sleep_ms(1000);
+
+        println!("{} is done eating.", self.name);
+    }
+}
+
+// Empty tuple used within this Mutex as we are not going to use (return)
+// the value, we are just going to hold on to it.
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
+fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
+    // Foucault is left-handed, solving the deadlocking problem.
+    let philosophers = vec![
+        Philosopher::new("Judith Butler", 0, 1),
+        Philosopher::new("Gilles Deleuze", 1, 2),
+        Philosopher::new("Karl Marx", 2, 3),
+        Philosopher::new("Emma Goldman", 3, 4),
+        Philosopher::new("Michel Foucault", 0, 4),
+    ];
+
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
+}
